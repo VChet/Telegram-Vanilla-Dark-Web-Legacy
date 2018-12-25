1e96a4b2f4645c30391532ddddbbd3de3ce04493
VChet <17050347+VChet@users.noreply.github.com>
2018-12-25 23:12:27 +0500

    Move gulp scripts to npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,14 @@
-const gulp = require('gulp');
-const stylelint = require('gulp-stylelint');
-const exec = require('child_process').exec;
+const gulp = require("gulp");
+const stylelint = require("gulp-stylelint");
 
-const usercss = "telegram-vanilla-dark.user.css";
-
-function lint() {
-  return gulp.src(
-    [
-      './**/*.css',
-      '!./node_modules/**'
-    ])
-    .pipe(stylelint({
-      reporters: [{
-        formatter: "string",
-        console: true
-      }]
-    }));
-}
-
-gulp.task('lint', function() {
-  return lint();
-});
-
-gulp.task('patch', function() {
-  lint();
-  return exec(`npx ver patch -p ${usercss}`);
-});
-
-gulp.task('minor', function() {
-  lint();
-  return exec(`npx ver minor -p ${usercss}`);
-});
-
-gulp.task('major', function() {
-  lint();
-  return exec(`npx ver major -p ${usercss}`);
-});
-
-gulp.task('usercss', function() {
-  return exec(`node ./options/build-usercss.js`);
+gulp.task("lint", function() {
+  return gulp.src([
+    "./**/*.css",
+    "!./node_modules/**"
+  ]).pipe(stylelint({
+    reporters: [{
+      formatter: "string",
+      console: true
+    }]
+  }));
 });
